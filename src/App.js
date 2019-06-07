0181aece4a51b4937652f271694ebff1f41b1817
serge-tj <serge@tommyjohn.com>
2019-06-07 05:48:06 -0400

    Initial rendering of data
    I am initially loading the data from my public folder I will work on rendering
    from the url after I can style the data data the way that I want

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Gallery from './components/gallery';
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Here we go</h1>
+      <div className="section">
+      <Gallery/>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
+/*TODO Please create a gallery that displays images
+
+[] 1. Must use this JSON as the data source JSON: https://drive.google.com/file/d/0B1jthw9CHq9zNWM2QkVlV1NvODdRd1AxbEpBTk1aZlpsRU5Z/view?usp=sharing 
+[] 2. The gallery should initially display 4 images on the page
+[] 3. The gallery should either 
+    a. lazily load more images from the JSON as the user scrolls 
+    b.  give the option to page to view the next or previous images in the gallery
+[] 4. Each image should load and have printed on the page to include 
+    a. DESCRIPTION
+    b. TAG
+    c. DATE 
+
+Bonus
+*/
---
+++ src/components/gallery.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: []
+    };
+  }
+  componentDidMount() {
+    axios.get("./api.json").then(({ data }) => {
+      console.log(data.rows);
+      this.setState({
+        images: data.rows
+      });
+    });
+  }
+
+  render() {
+    const { images } = this.state;
+    console.log("these are my images: ", images);
+    return (
+      <section className="gallery">
+        {images.map((image, index) => (
+          <div className="image-item" key={image.org.concat(index)}>
+            <img src={image.image} alt={image.tag} />
+            <p>{image.description}</p>
+          </div>
+        ))}
+      </section>
+    );
+  }
+}
